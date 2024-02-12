60c47e645ce20f47b4c65eb7300640d7e6c63740
Ahmed khaled <ahmedkh8907@gmail.com>
2024-02-12 12:52:03 +0200

    add svg pictuer

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:instagram_clone/core/utils/colors.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('data'),
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Flexible(flex: 2, child: Container()),
+          // svg image
+          SvgPicture.asset(
+            'assets/images/instagram.svg',
+            colorFilter: const ColorFilter.mode(
+              primaryColor,
+              BlendMode.srcIn,
+            ),
+            height: 64,
+            width: double.infinity,
+          ),
+          const SizedBox(height: 64),
+          //text field for email
+          const SizedBox(height: 64),
+
+          //text field for password
+          // login button
+          // transtioning to sign up
+        ],
+      ),
     );
   }
 }
