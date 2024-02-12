349ba0b2c92f1354724b9afc344452f01df7d60b
Ahmed khaled <ahmedkh8907@gmail.com>
2024-02-12 12:56:46 +0200

    create custom text field

--- lib/core/utils/widgets/custom_text_field.dart
+++ lib/core/utils/widgets/custom_text_field.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
+  final TextEditingController controller;
+  final String hintText;
+  final TextInputType keyboardType;
+  final bool isPass;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.keyboardType,
+    this.isPass = false,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const TextField(
-      decoration: InputDecoration(),
+    final inputBorder = OutlineInputBorder(
+      borderSide: Divider.createBorderSide(context),
+    );
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        contentPadding: const EdgeInsets.all(8),
+        hintText: hintText,
+        border: inputBorder,
+        focusedBorder: inputBorder,
+        enabledBorder: inputBorder,
+        filled: true,
+      ),
+      keyboardType: keyboardType,
+      obscureText: isPass,
     );
   }
 }
